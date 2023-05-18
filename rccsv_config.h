135f90016a1a0d2dcf78f5b4e71c7664a59785ec
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-19 06:26:34 +0700

    📦 Added rccsv_config.h

---
+++ rccsv_config.h
@@ -0,0 +1,19 @@
+/**
+ * @file rccsv_config.h
+ * @author UnknownRori (68576836+UnknownRori@users.noreply.github.com)
+ * @brief Global configuration for rccsv package
+ * @version 0.1
+ * @date 2023-05-18
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#pragma once
+
+#ifndef UNKNOWNRORI_RCCSV_CONFIG_H
+#define UNKNOWNRORI_RCCSV_CONFIG_H
+
+#define RCCSV_BUFFER_SIZE 2024
+
+#endif
