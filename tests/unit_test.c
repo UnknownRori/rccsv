07222c5d4027f7a26a541fac497071acec8124fd
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-25 05:54:29 +0700

    🛠️ Changes the unit test

--- tests/unit_test.c
+++ tests/unit_test.c
@@ -5,8 +5,30 @@
 
 #include "./test_framework.h"
 
-int main()
+#include "../src/FILE.h"
+
+TEST_CASE(read_entire_line);
+
+int main(int argc, char **argv)
 {
+    TEST_CASE_REGISTER(read_entire_line);
 
     return 0;
+}
+
+TEST_CASE(read_entire_line)
+{
+    TEST_CASE_INIT()
+
+    FILE *fp = fopen("./tests/data/dummy.txt", "r");
+
+    TEST_CASE_EXPECT(fp != nullptr, "It should open the file");
+
+    char *result = readline(fp);
+
+    TEST_CASE_EXPECT(strcmp(result, "AAAA") == 0, "It should be the same thing");
+
+    free(result);
+
+    TEST_CASE_DONE()
 }
