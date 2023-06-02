4cc2e1fb042e6b2dca942f4c6511f99bab7aac67
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-02 16:28:14 +0700

    📦 Added test case for rccsv headers

--- tests/unit_test.c
+++ tests/unit_test.c
@@ -5,15 +5,18 @@
 
 #include "./test_framework.h"
 
+#include "../include/UnknownRori/rccsv.h"
 #include "../src/FILE.h"
 
 #define TEST_DIR_APPEND(_PATH) "./tests/data/" _PATH
 
 TEST_CASE(read_entire_line);
+TEST_CASE(load_header_csv_1);
 
 int main(int argc, char **argv)
 {
     TEST_CASE_REGISTER(read_entire_line);
+    TEST_CASE_REGISTER(load_header_csv_1);
 
     return 0;
 }
@@ -33,4 +36,21 @@ TEST_CASE(read_entire_line)
     free(result);
 
     TEST_CASE_DONE()
+}
+
+TEST_CASE(load_header_csv_1)
+{
+    TEST_CASE_INIT();
+
+    const char *expected = "Id,Name,Birth Year";
+
+    rccsv *fd = rccsv_open(TEST_DIR_APPEND("first.csv"));
+
+    TEST_CASE_EXPECT(fd != nullptr, "It should open the file");
+
+    const char *result_header = rccsv_headers(fd);
+
+    TEST_CASE_EXPECT(strcmp(expected, result_header) == 0, "it should be the same thing");
+
+    TEST_CASE_DONE();
 }
